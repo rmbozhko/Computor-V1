260ea0bfd1e1153c1d012022fcb5e4bb982678d4
Roman Bozhko <rm.bozhko@gmail.com>
2018-08-31 10:12:11 +0300

    * Implemented own sqrt function, cause it's forbidden to use library one

--- Computor_add_funcs.cpp
+++ Computor_add_funcs.cpp
@@ -92,6 +92,7 @@ namespace Computor
 	void		PrintResults(const std::pair<double, double> res, const size_t polynomial_degree)
 	{
 		std::cout << "Polynomial degree: " << polynomial_degree << std::endl;
+		std::cout << "The solutions are:" << std::endl;
 		std::cout << res.first << std::endl;
 		std::cout << res.second << std::endl;
 	}
--- Computor_calc_equation.cpp
+++ Computor_calc_equation.cpp
@@ -7,27 +7,55 @@ namespace Computor
 		std::pair<double, double>		CalcDiscriminant(double a, double b, double c)
 		{
 			double	D, x1, x2;
+			auto sqrt = [=](const double number)
+			{
+				const double ACCURACY = 0.001;
+				double lower, upper, guess;
+
+				if (number < 1)
+				{
+					lower = number;
+					upper = 1;
+				}
+				else
+				{
+					lower = 1;
+					upper = number;
+				}
+
+				while ((upper - lower) > ACCURACY)
+				{
+					guess = (lower + upper) / 2;
+					if (guess * guess > number)
+						upper = guess;
+					else
+						lower = guess; 
+				}
+				return (lower + upper) / 2;
+			};
 
 			D = (b * b) - (4 * a * c);
-			if (D > 0)
+			
+			if (D > 0.0)
 			{
 				std::cout << "Discriminant is greater than zero" << std::endl;
-				x1 = (-b + std::sqrt(D)) / (2 * a);
-				x2 = (-b - std::sqrt(D)) / (2 * a);
+				x1 = (-b + sqrt(D)) / (2 * a);
+				x2 = (-b - sqrt(D)) / (2 * a);
 			}
-			else if (D == 0)
+			else if (D == 0.0)
 			{
 				std::cout << "Discriminant is equal to zero" << std::endl;
-				x1 = x2 = (-b + std::sqrt(D)) / (2 * a);
+				x1 = x2 = (-b + sqrt(D)) / (2 * a);
 			}
 			else
 			{
 				double realPart = -b / (2 * a);
-		        double imaginaryPart = std::sqrt(-D) / ( 2 * a);
+		        double imaginaryPart = sqrt(-D) / ( 2 * a);
 		        std::cout << realPart << " + " << imaginaryPart << "i" << std::endl;
 		        std::cout << realPart << " - " << imaginaryPart << "i" << std::endl;
 		        throw std::string("Discriminant is less than zero");
 		    }
+			
 			return (std::make_pair(x1, x2));
 		}
 
